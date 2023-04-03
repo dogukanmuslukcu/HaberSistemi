e0ccb105efd950b67beade61ab744a646a31bcf5
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-04-03 11:56:10 +0300

    ResimRepository

--- HaberSistemi.Core/Repository/HaberRepository.cs
+++ HaberSistemi.Core/Repository/HaberRepository.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using HaberSistemi.Core.Infrastructure;
+using HaberSistemi.Data.DataContext;
+using HaberSistemi.Data.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HaberSistemi.Core.Repository
 {
-    internal class HaberRepository
+    public class HaberRepository : IHaberRepository
     {
+       
+        private readonly HaberContext context = new HaberContext();
+
+        public IEnumerable<Haber> GetAll()
+        {
+            return context.Haber.Select(x => x); 
+        }
+        public Haber GetById(int id)
+        {
+            return  context.Haber.FirstOrDefault( x=> x.ID == id);
+        }
+
+
+        public Haber Get(Expression<Func<Haber, bool>> expression)
+        {
+            return context.Haber.FirstOrDefault(expression);
+        }
+
+
+        public IQueryable<Haber> GetMany(Expression<Func<Haber, bool>> expression)
+        {
+            return context.Haber.Where(expression);
+
+        }
+
+        public void Insert(Haber Obj)
+        {
+           context.Haber.Add(Obj);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(Haber Obj)
+        {
+           context.Haber.AddOrUpdate(Obj);
+        }
+        public int Count()
+        {
+            return context.Haber.Count();
+        }
+
+        public void Delete(int id)
+        {
+             var haber = GetById(id);
+            if (haber != null) 
+            {
+                context.Haber.Remove(haber);
+            
+            }
+        }
+
+       
     }
 }
---
+++ HaberSistemi.Core/Repository/ResimRepository.cs
@@ -0,0 +1,68 @@
+﻿using HaberSistemi.Core.Infrastructure;
+using HaberSistemi.Data.DataContext;
+using HaberSistemi.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaberSistemi.Core.Repository
+{
+    public class ResimRepository : IResimRepository
+    {
+        private readonly HaberContext context = new HaberContext();
+
+       
+
+        public IEnumerable<Resim> GetAll()
+        {
+            return context.Resim.Select(x => x);
+        }
+
+        public Resim GetById(int id)
+        {
+            return context.Resim.FirstOrDefault(x => x.ID == id);
+        }
+        public Resim Get(Expression<Func<Resim, bool>> expression)
+        {
+            return context.Resim.FirstOrDefault(expression);
+        }
+
+        public IQueryable<Resim> GetMany(Expression<Func<Resim, bool>> expression)
+        {
+            return context.Resim.Where(expression);
+        }
+
+        public void Insert(Resim Obj)
+        {
+            context.Resim.Add(Obj);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(Resim Obj)
+        {
+            context.Resim.AddOrUpdate(Obj);
+        }
+        public int Count()
+        {
+            return context.Resim.Count();
+        }
+
+        public void Delete(int id)
+        {
+            var resim = GetById(id);
+            if (resim != null)
+            {
+                context.Resim.Remove(resim);
+
+            }
+        }
+    }
+}
