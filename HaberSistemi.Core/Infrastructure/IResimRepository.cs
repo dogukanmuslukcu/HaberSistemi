123184f43b1adff637c67ba8d25663f5cd29bfee
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-04-03 12:37:33 +0300

    IResimRepository

--- HaberSistemi.Core/Infrastructure/IResimRepository.cs
+++ HaberSistemi.Core/Infrastructure/IResimRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HaberSistemi.Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HaberSistemi.Core.Infrastructure
 {
-    internal interface IResim
+    public interface IResimRepository : IRepository<Resim>
     {
     }
 }
