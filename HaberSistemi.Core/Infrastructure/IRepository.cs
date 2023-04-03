7018b47707c78d3aa0fa5c7457ffbb448e3a097a
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-04-03 11:40:28 +0300

    HaberRepository

--- HaberSistemi.Core/Infrastructure/IRepository.cs
+++ HaberSistemi.Core/Infrastructure/IRepository.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HaberSistemi.Core.Infrastructure
 {
-    internal interface IRepository
+    public interface IRepository<T> where T :class
     {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        T Get(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetMany(Expression<Func<T, bool>> expression);
+        void Insert(T Obj);
+        void Update(T Obj);
+        void Delete(int id);
+        int Count();
+        void Save();
+
+
     }
 }
