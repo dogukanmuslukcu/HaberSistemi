a638da552391a532edffd874ea887ebcbede1153
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-05-14 17:23:49 +0300

    KategoriController

---
+++ HaberSistemi.Admin/Controllers/KategoriController.cs
@@ -0,0 +1,24 @@
+﻿using HaberSistemi.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaberSistemi.Admin.Controllers
+{
+    public class KategoriController : Controller
+    {
+        #region Kategori
+        private readonly IKategoriRepository _kategoriRepository;
+        public KategoriController(IKategoriRepository kategoriRepository) 
+        {
+            _kategoriRepository = kategoriRepository;
+        }
+        #endregion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
