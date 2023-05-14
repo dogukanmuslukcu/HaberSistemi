a6c43c31a540dd2bb2b1c9fa1add31b49d45ff70
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-05-14 16:37:24 +0300

    AutomaticMigration= True

--- HaberSistemi.Data/Migrations/Configuration.cs
+++ HaberSistemi.Data/Migrations/Configuration.cs
@@ -9,7 +9,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(HaberSistemi.Data.DataContext.HaberContext context)
